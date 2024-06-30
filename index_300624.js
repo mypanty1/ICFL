08b61ae66a89a1c1fbfc695969ee10f22697582a
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-06-30 19:10:17 +0700

    Create index_300624.js

---
+++ index_300624.js
@@ -0,0 +1,33 @@
+if(!window.AppInventor){
+  window.AppInventor = {
+    setWebViewString: function(...args){
+      if(!window.flutter_inappwebview?.callHandler){return}
+      return window.flutter_inappwebview.callHandler('setWebViewString', ...args)
+    },
+    getWebViewString: function(...args){
+      return window.flutter_inappwebview.callHandler('getWebViewString', ...args)
+    },
+  };
+};
+
+//port refree
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/AbonPortRefree.js',type:'text/javascript'}));
+//SitePlanDownload
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SiteNodeDuExt.js',type:'text/javascript'}));
+//SendKionPq
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SendKionPq.js',type:'text/javascript'}));
+//SiteLinkChangeTraps
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SiteLinkChangeTraps.js',type:'text/javascript'}));
+
+//add port disable approve dialog
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/PortActionDisableApproveModal.js',type:'text/javascript'}));
+
+
+
+
+
+
+
+
+
+
