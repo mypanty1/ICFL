1924a49ab19bb10aa091114761d6f0937e7e1c8c
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-06 00:18:12 +0700

    Update index_060724.js

--- index_060724.js
+++ index_060724.js
@@ -17,7 +17,7 @@ if(!window.AppInventor){
   };
 };
 
-//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/app-update-notifer.js',type:'text/javascript'}));
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/app-update-notifer.js',type:'text/javascript'}));
 
 //port refree
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/AbonPortRefree.js',type:'text/javascript'}));
