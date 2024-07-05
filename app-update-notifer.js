092bc72ebee48425b51c5831b249da1101eafccd
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-05 16:30:15 +0700

    Update app-update-notifer.js

--- app-update-notifer.js
+++ app-update-notifer.js
@@ -1,4 +1,10 @@
 setInterval(() => {
-  const message = `доступно обновление.\nобновить можно по той-же ссылке`;
+  const message = [
+    'доступно обновление.',
+    'обновить можно по той-же ссылке',
+    '',
+    'ICFL v0.1.3',
+    '*восстановлена работа кнопки назад'
+  ].join('\n');
   window.flutter_inappwebview.callHandler('showSnackBar', message);
 }, 22222);
