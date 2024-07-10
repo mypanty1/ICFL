013e83f37ffd96c5b44dddf98361a04283eabe89
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-10 15:32:19 +0700

    Update index_070724.js

--- index_070724.js
+++ index_070724.js
@@ -4,27 +4,6 @@ if(app){
   window.flutter_inappwebview.callHandler('setUserLogin', app.$store.getters.userLogin);
 };
 
-if(!window.AppInventor){
-  window.AppInventor = {
-    setWebViewString: function(...args){
-      return window.flutter_inappwebview.callHandler('setWebViewString', ...args)
-    },
-    getWebViewString: function(...args){
-      return window.flutter_inappwebview.callHandler('getWebViewString', ...args)
-    },
-  };
-};
-if(app?.$store?.getters?.userLogin == 'mypanty1'){
-  document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/app-update-notifer.js',type:'text/javascript'}));
-};
-//port refree
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/AbonPortRefree.js',type:'text/javascript'}));
-//SitePlanDownload
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SiteNodeDuExt.js',type:'text/javascript'}));
-//SendKionPq
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SendKionPq.js',type:'text/javascript'}));
-//SiteLinkChangeTraps
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/SiteLinkChangeTraps.js',type:'text/javascript'}));
+//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/app-update-notifer.js',type:'text/javascript'}));
 
-//add port disable approve dialog
-document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/PortActionDisableApproveModal.js',type:'text/javascript'}));
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/addons.js',type:'text/javascript'}));
