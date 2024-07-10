5a6f89befeb2060c9d0b8ee69450121b30ea2649
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-10 15:30:14 +0700

    Update addons.js

--- addons.js
+++ addons.js
@@ -1,2 +1,12 @@
+if(!window.AppInventor){
+  window.AppInventor = {
+    setWebViewString: function(...args){
+      return window.flutter_inappwebview.callHandler('setWebViewString', ...args)
+    },
+    getWebViewString: function(...args){
+      return window.flutter_inappwebview.callHandler('getWebViewString', ...args)
+    },
+  };
+};
 
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/FX_test/addons.js',type:'text/javascript'}));
