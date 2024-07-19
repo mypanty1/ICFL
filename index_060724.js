4b978ef9a12db18fb539f7cf1266e99458518c48
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-20 00:38:50 +0700

    Update index_060724.js

--- index_060724.js
+++ index_060724.js
@@ -6,6 +6,6 @@ if(app){
 
 //window.flutter_inappwebview.callHandler('launchUrl', 'https://www.youtube.com/');
 
-//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/app-update-notifer.js',type:'text/javascript'}));
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/app-update-notifer.js',type:'text/javascript'}));
 
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/addons.js',type:'text/javascript'}));
