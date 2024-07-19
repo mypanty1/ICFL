2de01d11305f7fc84c02f66befe5b48554717aa7
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-20 00:44:03 +0700

    Update 190724.js

--- 190724.js
+++ 190724.js
@@ -7,3 +7,4 @@ if(app){
 //document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/app-update-notifer.js',type:'text/javascript'}));
 
 document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/addons.js',type:'text/javascript'}));
+
