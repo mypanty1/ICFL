3d272192091ddce47dd3996e5578814550b9b6f2
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-20 01:14:25 +0700

    Update app-update-notifer.js

--- app-update-notifer.js
+++ app-update-notifer.js
@@ -4,12 +4,13 @@ setInterval(() => {
     'обновить можно по той-же ссылке',
     '',
     'ICFL v0.1.5',
-    '*экран не гаснет'
+    //'*экран не гаснет'
   ].join('\n');
   window.flutter_inappwebview.callHandler('showSnackBar', message);
   app.$store.dispatch('toast/open',{
     iconName:'SqCheckedFill',
     iconColor:'#26CD58',
     messageText: message,
+    duration: 11111
   });
 }, 22222);
