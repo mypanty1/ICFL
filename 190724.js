422611e76b5faf0c48325b284491bf81c1628e1a
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-19 23:31:24 +0700

    Create 190724.js

---
+++ 190724.js
@@ -0,0 +1,9 @@
+window.flutter_inappwebview.callHandler('setJsFileVersion', '190724');
+//window.flutter_inappwebview.callHandler('showSnackBar', 'в этом предложении тридцать две буквы');
+if(app){
+  window.flutter_inappwebview.callHandler('setUserLogin', app.$store.getters.userLogin);
+};
+
+//document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/app-update-notifer.js',type:'text/javascript'}));
+
+document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://mypanty1.github.io/ICFL/addons.js',type:'text/javascript'}));
