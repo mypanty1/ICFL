dc2afe3fee0ea4d43f0a98d84cb5b7f3e8a3df74
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-21 21:12:34 +0700

    Update app-update-notifer.js

--- app-update-notifer.js
+++ app-update-notifer.js
@@ -1,16 +1,27 @@
 setInterval(() => {
-  const message = [
-    'доступно обновление.',
-    'обновить можно по той-же ссылке',
-    '',
-    'ICFL v0.1.5',
-    //'*экран не гаснет'
-  ].join('\n');
+  // const message = [
+  //   'доступно обновление.',
+  //   'обновить можно по той-же ссылке',
+  //   '',
+  //   'ICFL v0.1.5',
+  //   '*экран не гаснет'
+  // ].join('\n');
   //window.flutter_inappwebview.callHandler('showSnackBar', message);
-  app.$store.dispatch('toast/open',{
-    iconName:'SqCheckedFill',
-    iconColor:'#26CD58',
-    messageText: message,
-    duration: 9999
-  });
+  if(store.getters.userLogin === 'mypanty1'){
+    store.dispatch('toast/open', {
+      iconName:'SqInfo',
+      iconColor:'#26CD58',
+      duration: 9999,
+      messageText: 'доступно обновление ICFL v0.1.5',
+      actionText: 'Download',
+      actionOnClick: store.dispatch('app/actionView', 'https://ping54.ru/shared/ICFL/app-release.apk'),
+    });
+  }else{
+    app.$store.dispatch('toast/open',{
+      iconName:'SqCheckedFill',
+      iconColor:'#26CD58',
+      messageText: 'доступно обновление. обновить можно по той-же ссылке ICFL v0.1.5',
+      duration: 9999
+    });
+  }
 }, 11111);
