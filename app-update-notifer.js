b99da2c7e65d6cca585f7763525965b372e732f3
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-21 21:28:18 +0700

    Update app-update-notifer.js

--- app-update-notifer.js
+++ app-update-notifer.js
@@ -12,7 +12,8 @@ setInterval(() => {
       iconName:'SqInfo',
       iconColor:'#26CD58',
       duration: 9999,
-      messageText: 'доступно обновление ICFL v0.1.5',
+      messageText: 'доступно обновление\nICFL v0.1.5',
+      messageClass: 'white-space-pre-wrap',
       actionText: 'Download',
       actionOnClick: () => window.flutter_inappwebview.callHandler('launchUrl', 'https://ping54.ru/shared/ICFL/app-release.apk'),
     });
