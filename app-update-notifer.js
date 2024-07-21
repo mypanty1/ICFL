232072e02015b664be69455ef323f4e930282528
mypanty1 <54975980+mypanty1@users.noreply.github.com>
2024-07-21 21:24:30 +0700

    Update app-update-notifer.js

--- app-update-notifer.js
+++ app-update-notifer.js
@@ -14,7 +14,7 @@ setInterval(() => {
       duration: 9999,
       messageText: 'доступно обновление ICFL v0.1.5',
       actionText: 'Download',
-      actionOnClick: window.flutter_inappwebview.callHandler('launchUrl', 'https://ping54.ru/shared/ICFL/app-release.apk'),
+      actionOnClick: () => window.flutter_inappwebview.callHandler('launchUrl', 'https://ping54.ru/shared/ICFL/app-release.apk'),
     });
   }else{
     app.$store.dispatch('toast/open',{
